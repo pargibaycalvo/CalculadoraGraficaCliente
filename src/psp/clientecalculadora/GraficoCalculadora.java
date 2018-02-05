f57fca7e5a754c4fea661384753fd7ca8b3a72df
Pedro José Argibay Calvo <pdrjs616@gmail.com>
2018-02-05 12:55:48 +0100

    Conectada con el Server

--- src/psp/clientecalculadora/GraficoCalculadora.java
+++ src/psp/clientecalculadora/GraficoCalculadora.java
@@ -1,5 +1,9 @@
 package psp.clientecalculadora;
 
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -20,6 +24,16 @@ public class GraficoCalculadora extends javax.swing.JFrame {
      */
     public GraficoCalculadora() {
         initComponents();
+    }
+    
+    public static void conectaServer() throws IOException{
+            System.out.println("Creando ClienteSocket");
+            Socket clienteSocket = new Socket();
+            System.out.println("Estableciendo la conexión");
+            InetSocketAddress addr = new InetSocketAddress("localhost", 5555);
+            clienteSocket.connect(addr);
+            System.out.println("Conexión establecida con éxito");
+            System.out.println("Mostrando Calculadora");
     }
 
     /**
@@ -387,29 +401,6 @@ public class GraficoCalculadora extends javax.swing.JFrame {
      * @param args the command line arguments
      */
     public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(GraficoCalculadora.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(GraficoCalculadora.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(GraficoCalculadora.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(GraficoCalculadora.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }
-        //</editor-fold>
-
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
